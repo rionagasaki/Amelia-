ff0b1da5bd3151744eba8686c53ded15c627cc55
rio.nagasaki <naga_ri@icloud.com>
2022-05-27 16:43:19 +0900

    beta version

---
+++ Instagram/Login/AppleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppleViewController.swift
+//  Instagram
+//
+//  Created by Rio Nagasaki on 2021/12/21.
+//
+
+import UIKit
+
+class AppleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
